39a83de6f125fd15dcfdfc10ac7ee4363fe1bf9b
Justin Sackett <jsackett@lyft.com>
2022-11-17 21:54:32 -0600

    resubmission changes

--- etl.py
+++ etl.py
@@ -6,61 +6,73 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
-    # open song file
+    """This function processes the data from the song .json files and inserts the records into the `songs` and `artists` tables, respectively.
+    """
+    # open song file using pandas read_json method, save as a dataframe
     df = pd.read_json(filepath, lines=True)
 
-    # insert song record
+    # select fields to be added to the songs table, extract values from dataframe, and execute the insert command
     song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0]
     cur.execute(song_table_insert, song_data)
-    
-    # insert artist record
+
+    # select fields to be added to the artists table, extract values from dataframe, and execute the insert command
     artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0]
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
-    # open log file
+    """This function processes the data from the log .json files and inserts the records into the `time`, `user`, and `songplay` tables, respectively.
+    """
+    # open log file using pandas read_json method, save as a dataframe
     df = pd.read_json(filepath, lines=True)
 
-    # filter by NextSong action
+    # filter dataframe for records with page == 'NextSong'
     df = df[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
     t = pd.to_datetime(df['ts'], unit='ms')
-    
-    # insert time data records
+
+    # insert time data records using pandas dt attributes
     time_data = (t, t.dt.hour, t.dt.day, t.dt.weekofyear, t.dt.month, t.dt.year, t.dt.weekday)
     column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
     time_df = pd.DataFrame.from_dict(dict(zip(column_labels, time_data)))
 
+    # add rows incrementally to the `time` table
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
-    # load user table
+    # select fields to be added to the users table, extract values from dataframe, and execute the insert command
     user_df = df[['userId','firstName','lastName','gender','level']]
 
-    # insert user records
+    # add rows incrementally to the `time` table
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
 
-    # insert songplay records
+    # add rows incrementally to the `songplays` table
     for index, row in df.iterrows():
-        
-        # get songid and artistid from song and artist tables
+
+        # get songid and artistid from song and artist tables -- these don't exist on the raw log files, so we'll need to query for associated songs/artists based on song title, artist name, and song duration
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
+
+        # if the above query finds a song/artist match, save them to the variables songid and artistid. Otherwise, set these variables to None
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = (index, pd.to_datetime(row.ts, unit='ms'), row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        songplay_data = (pd.to_datetime(row.ts, unit='ms'), row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
+    """This function has the following structure:
+    1) Loop through all subfolders in the data folder, find all .json files, and save their filespaths to a list called `all_files`.
+    2) Print file count to console
+    3) Apply the function argument (func -- either process_song_file or process_log_file defined above) to files in all_files iteratively (which commits data to the appropriate tables)
+    4) Print file process success note as data is committed
+    """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -80,6 +92,12 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    """This is the main function that runs all process defined above:
+    1) Connect to the sparkifydb and initiate a cursor
+    2) Apply the process_data function to the files in the song_data subfolder, using the process_song_file function defined above
+    3) Apply the process_data function to the files in the log_data subfolder, using the process_log_file function defined above
+    4) Close the connection for security
+    """
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
@@ -90,4 +108,5 @@ def main():
 
 
 if __name__ == "__main__":
+    """Run the script"""
     main()
--- sql_queries.py
+++ sql_queries.py
@@ -9,13 +9,13 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY,
-start_time TIMESTAMP NOT NULL,
-user_id BIGINT NOT NULL,
+start_time TIMESTAMP NOT NULL REFERENCES time(start_time),
+user_id BIGINT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
 level VARCHAR,
-song_id VARCHAR,
-artist_id VARCHAR,
+song_id VARCHAR REFERENCES songs(song_id) ON DELETE CASCADE,
+artist_id VARCHAR REFERENCES artists(artist_id) ON DELETE CASCADE,
 session_id INT,
-location VARCHAR, 
+location VARCHAR,
 user_agent VARCHAR)
 """)
 
@@ -40,7 +40,7 @@ latitude DOUBLE PRECISION,
 longitude DOUBLE PRECISION)
 """)
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time TIMESTAMP,
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time TIMESTAMP PRIMARY KEY,
 hour INT,
 day INT,
 week INT,
@@ -51,29 +51,39 @@ weekday INT)
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (songplay_id) DO NOTHING
+songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (songplay_id) DO NOTHING
 """)
 
-user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING 
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
 """)
 
-song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO NOTHING 
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id) DO NOTHING
 """)
 
-artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id) DO NOTHING
 """)
 
 
-time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+VALUES (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (start_time) DO NOTHING
 """)
 
 # FIND SONGS
 
-song_select = ("""select s.song_id, a.artist_id from songs s join artists a on s.artist_id=a.artist_id 
+song_select = ("""select s.song_id, a.artist_id from songs s join artists a on s.artist_id=a.artist_id
 where s.title = %s and a.name = %s and s.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [user_table_drop, song_table_drop, artist_table_drop, time_table_drop,songplay_table_drop]
